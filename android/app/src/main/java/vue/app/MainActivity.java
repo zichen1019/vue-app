9a4efcb6cfccc5715cd40fe6df840cc51021adda
e <1510748736@qq.com>
2024-07-02 22:49:20 +0800

    'init'

---
+++ android/app/src/main/java/vue/app/MainActivity.java
@@ -0,0 +1,5 @@
+package vue.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'vue.app',
+  appName: 'vue-app',
+  webDir: 'dist'
+};
+
+export default config;
